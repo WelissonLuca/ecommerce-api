2f1fbac5c8e820485a8f97a1f99499df67c1176c
Welisson luca <welissonluca17@gmail.com>
2021-06-01 17:35:27 -0300

    chore: add git-commit-linter

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 
 module.exports = {
 	async create(req, res) {
-		
+		const produto = [];
+
+		produto.push({
+			name: "dsadad22s",
+			produto: "sada2222sd",
+    });
+    return res.send('cheguei aqui');
 	},
 };
