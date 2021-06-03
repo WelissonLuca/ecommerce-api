26e03d3aea904dd1e55d50cd943fb0ecb2a8e9aa
inglyd <inglydalves@outlook.com>
2021-06-03 11:55:14 -0300

    feat: get, get{id} e post

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,8 +3,9 @@ const controller = require('../controllers/Products')
 
 const productsRouter = express.Router();
 
-
-productsRouter.post('/', controller.create)
+productsRouter.get('/products', controller.list)
+productsRouter.get('/product/:id', controller.show)
+productsRouter.post('/products', controller.create)
 
 
 module.exports = { productsRouter };
